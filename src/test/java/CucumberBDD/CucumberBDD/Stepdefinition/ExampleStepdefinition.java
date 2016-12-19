1455ade267fbc4dd9fae1b4eed9368b5d42b1ebb
kailash <kailash@kailash-PC>
2016-12-19 07:43:35 +0530

    sample project

---
+++ src/test/java/CucumberBDD/CucumberBDD/Stepdefinition/ExampleStepdefinition.java
@@ -0,0 +1,12 @@
+package CucumberBDD.CucumberBDD.Stepdefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.thucydides.core.annotations.Steps;
+
+public class ExampleStepdefinition {
+
+	
+    }
+
